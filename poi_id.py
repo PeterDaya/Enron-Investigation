fee832c10c52c8318017bd51c0c5c060f2d01548
Peter Daya <pdaya282@gmail.com>
2017-07-26 11:05:18 -0400

    Removed additional features

--- poi_id.py
+++ poi_id.py
@@ -10,37 +10,11 @@ from sklearn import tree
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
 from sklearn.cross_validation import train_test_split
-finance_features = [
-        'salary', 
-        'deferral_payments', 
-        'total_payments', 
-        'loan_advances', 
-        'bonus', 
-        'restricted_stock_deferred', 
-        'deferred_income', 
-        'total_stock_value', 
-        'expenses', 
-        'exercised_stock_options', 
-        'other', 
-        'long_term_incentive', 
-        'restricted_stock', 
-        'director_fees'
-        ] 
-email_features = [
-        'to_messages', 
-        'email_address', 
-        'from_poi_to_this_person', 
-        'from_messages', 
-        'from_this_person_to_poi', 
-        'shared_receipt_with_poi'
-        ] 
-
-poi = ['poi']
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = poi + finance_features + email_features  # You will need to use more features
+features_list = ["poi", "salary"]# You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
