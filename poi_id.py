bbbd47d082a4f6a6de8a3088a00847afd1e3a2d7
Peter Daya <pdaya282@gmail.com>
2017-07-31 17:21:04 -0400

    Deleted some comments and changed formatting

--- poi_id.py
+++ poi_id.py
@@ -41,11 +41,11 @@ def plot_data(data_dict, plot_x, plot_y):
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ["poi", "salary", 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 
-'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options',
-'long_term_incentive', 'restricted_stock', 'director_fees',
-'to_messages', 'from_poi_to_this_person', 'from_messages', 
-'from_this_person_to_poi', 'shared_receipt_with_poi']
+features_list = ["poi", "salary", "deferral_payments", "total_payments", "loan_advances", "bonus", 
+"restricted_stock_deferred", "deferred_income", "total_stock_value", "expenses", "exercised_stock_options",
+"long_term_incentive", "restricted_stock", "director_fees",
+"to_messages", "from_poi_to_this_person", "from_messages", 
+"from_this_person_to_poi", "shared_receipt_with_poi"]
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -53,13 +53,13 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 ### Task 2: Remove outliers
 print "Number of data points before removing outliers: ", len(data_dict)
-outliers = ['TOTAL', 'LOCKHART EUGENE E', 'THE TRAVEL AGENCY IN THE PARK']
+outliers = ["TOTAL", "LOCKHART EUGENE E", "THE TRAVEL AGENCY IN THE PARK"]
 for i in range(len(outliers)):
     data_dict.pop(outliers[i])
 
 cp = dict(data_dict)
 for person in cp:
-    if (cp[person]['salary'] == 'NaN') or (cp[person]['bonus'] == 'NaN'):
+    if (cp[person]["salary"] == "NaN") or (cp[person]["bonus"] == "NaN"):
         data_dict.pop(person)
 
 print "Number of data points after removing outliers: ", len(data_dict)
@@ -71,8 +71,8 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-plot_data(data_dict, 'salary', 'bonus')
-plot_data(data_dict, 'salary',  'from_poi_to_this_person')
+plot_data(data_dict, "salary", "bonus")
+plot_data(data_dict, "salary",  "from_poi_to_this_person")
 
 for i in my_dataset:
     record = my_dataset[i]
@@ -98,29 +98,17 @@ selected_features = dict(sorted_info[:10])
 features_list = ["poi"] + selected_features.keys()
 
 ### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# Provided to give you a starting point. Try a variety of classifiers.
 clf = GaussianNB()
-
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
 ### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
 clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 print "Accuracy Score: ", accuracy_score(pred, labels_test)
